bc92bf0c57e3056677db6d5a90671a0507b6bab3
HEMOC100 <hemoc100@hcfmb.ad>
2022-01-24 23:30:17 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//Crie um array que receba 5 itens e exiba no console.
+let comidas = ["lasanha", "strogonoff", "bife", "macarronada", "fricasse"]
+console.log (comidas)
+
+//Utilize um método para adicionar um nome ao inicio do array.
+comidas.unshift("arroz")
+console.log (comidas)
+
+//Utilize um método para remover o último nome do array.
+comidas.pop()
+console.log(comidas)
+
+//Utilize um método para adicionar dois nomes ao fim do array.
+comidas.push("arroz","feijão")
+console.log(comidas)
+
+//Utilize um método para remover o primeiro nome do array.
+comidas.shift()
+console.log (comidas)
+
+//Utilize um método para organizar em ordem crescente o seguinte array: const numbers = [7,5,6,3,8,9,2,1,4]
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
